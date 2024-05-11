1d2557463aa65506c03657ee1b5325b47ec9fdae
zhangweiweicpp <2430290933@qq.com>
2024-05-11 02:03:04 +0000

    0511

---
+++ day01/build/CMakeFiles/01.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "01"
+  "01.pdb"
+  "CMakeFiles/01.dir/main.cpp.o"
+  "CMakeFiles/01.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
